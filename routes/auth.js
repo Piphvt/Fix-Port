a10dc6da75c73972fc1830b27f8799da76d69fe5
piphat <Piphat.upachatai@gmail.com>
2024-11-20 13:48:12 +0700

    สร้างไฟล์ครั้งแรก

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, refresh, logout, register, forgotPassword, verifyOTP, resetPassword } = require('../controller/auths.js');
+const { login, refresh, logout, register } = require('../controller/auths.js');
 
 router.post('/login', login);
 router.get('/refresh', refresh);
 router.delete('/logout', logout);
 router.post('/register', register);
-router.post('/forgot-password', forgotPassword);
-router.post('/verify-otp', verifyOTP);
-router.post('/reset-password', resetPassword);
-
 module.exports = router;
