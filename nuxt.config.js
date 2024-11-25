f374cb29459c43048134d423e8afca374808f325
piphat <Piphat.upachatai@gmail.com>
2024-11-25 11:25:46 +0700

    อัพเดทล่าสุด

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'static',
-  buildDir: 'dist',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - AI Quant Tech Co.,Ltd',
